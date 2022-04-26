a4ab4b6ac173336bc44c83bdf2046b826c870adc
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-04-26 20:51:57 -0300

    Primeiros passos para desenvolvimento web

---
+++ Primeiros passos para desenvolvimento web/index.php
@@ -0,0 +1,17 @@
+<html>
+
+	<header>
+		<title>Meu primeiro site em php</title>
+	</header>
+	
+	<body>
+		
+		<?php
+			for($i = 0; $i < 10; $i++ ){
+				print( "Linha numero" .$i . "<br />");
+			}
+		?>
+		
+	</body>
+
+</html>
